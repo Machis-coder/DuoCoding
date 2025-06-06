8c2c51a9d8cef3156c2066dda0752130d806e8dc
david <lekhardev@gmail.com>
2025-06-06 12:15:39 +0200

    Creados SubjectService, SubjectRepository. Actualizado SubjecController

---
+++ duocoding/src/main/java/com/codingtrainers/duocoding/repositories/SubjectRepository.java
@@ -0,0 +1,38 @@
+package com.codingtrainers.duocoding.repositories;
+
+import com.codingtrainers.duocoding.entities.Subject;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SubjectRepository {
+
+    private List<Subject> subjects;
+
+    @PostConstruct
+    public void init() {
+        subjects = new ArrayList<>();
+        subjects.add(new Subject("Java", 1, "Java Subject"));
+        subjects.add(new Subject("Databases", 2, "Databases Subject"));
+        subjects.add(new Subject("Frontend", 3, "Frontend Subject"));
+        subjects.add(new Subject("Devops", 4, "Devops Subject"));
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public Subject findById(int id) {
+        for (int i = 0; i < subjects.size(); i++) {
+            Subject subject = subjects.get(i);
+            if (subject.getId() == id) {
+                return subject;
+            }
+        }
+        return null;
+    }
+
+}
