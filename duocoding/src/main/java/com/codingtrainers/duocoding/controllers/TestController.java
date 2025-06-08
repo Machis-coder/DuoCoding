50e9c4c5a3374497646df5539f5982c000777621
david <lekhardev@gmail.com>
2025-06-08 11:10:53 +0200

    Creados Controller, Service y Repository para Test y para Test

--- duocoding/src/main/java/com/codingtrainers/duocoding/controllers/TestController.java
+++ duocoding/src/main/java/com/codingtrainers/duocoding/controllers/TestController.java
@@ -3,10 +3,8 @@ package com.codingtrainers.duocoding.controllers;
 import com.codingtrainers.duocoding.entities.Test;
 import com.codingtrainers.duocoding.services.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,13 +15,31 @@ public class TestController {
     @Autowired
     private TestService testService;
 
-    @GetMapping("/")
+    @GetMapping
     public List<Test> getAllTests() {
-        return testService.getAll();
+        return testService.getAllTests();
     }
 
     @GetMapping("/{id}")
-    public Test getTestById(@PathVariable int id) {
-        return testService.getById(id);
+    public Test getTestById(@PathVariable Long id) {
+        return testService.getTestById(id);
+    }
+
+    @PostMapping
+    public Test createTest(@RequestBody Test test) {
+        return testService.createTest(test);
+    }
+
+    @PutMapping("/{id}")
+    public Test updateTest(@PathVariable Long id, @RequestBody Test test) {
+        test.setId(id);
+        return testService.updateTest(test);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTest(@PathVariable Long id) {
+        String message = testService.deleteTestById(id);
+        return ResponseEntity.ok(message);
     }
 }
+
