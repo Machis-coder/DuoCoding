f39589c1aef5336f4bb5c28d7ea210ec9fb91017
machis-coder <antogm0997@gmail.com>
2025-06-10 20:29:03 +0200

    arreglados desperfectos y creados cruds

---
+++ duocoding/src/main/java/com/codingtrainers/duocoding/services/TestExecutionService.java
@@ -0,0 +1,54 @@
+package com.codingtrainers.duocoding.services;
+
+import com.codingtrainers.duocoding.entities.Test;
+import com.codingtrainers.duocoding.entities.TestExecution;
+import com.codingtrainers.duocoding.entities.User;
+import com.codingtrainers.duocoding.repositories.TestExecutionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TestExecutionService {
+
+    @Autowired
+    private TestExecutionRepository testExecutionRepository;
+
+    public List<TestExecution> getTestExecutions() {
+        return testExecutionRepository.findAll();
+    }
+
+    public List<TestExecution> getTestExecutionByUser(Long userId) {
+        User user = new User();
+        user.setId(userId);
+        return testExecutionRepository.findByUser(user);
+    }
+
+    public List<TestExecution> getTestExecutionByTest(Long  testId) {
+        Test test = new Test();
+        test.setId(testId);
+        return testExecutionRepository.findByTest(test);
+    }
+    public void deleteTestExecution(Long testExecutionId) {
+        testExecutionRepository.deleteById(testExecutionId);
+    }
+
+    public Optional<TestExecution> getTestExecutionById(Long id) {
+        return testExecutionRepository.findById(id);
+    }
+
+    public TestExecution saveTestExecution(Long userId, Long testId)
+    {
+        User user = new User();
+        user.setId(userId);
+        Test test = new Test();
+        test.setId(testId);
+        TestExecution testExecution = new TestExecution();
+        testExecution.setUser(user);
+        testExecution.setTest(test);
+        return testExecutionRepository.save(testExecution);
+    }
+
+}
