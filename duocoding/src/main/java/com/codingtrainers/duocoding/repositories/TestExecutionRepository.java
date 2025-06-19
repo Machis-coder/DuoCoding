dd8617adcc407b3bc7c6a44c4d6cdbfd4584c0d7
machis-coder <antogm0997@gmail.com>
2025-06-19 16:52:14 +0200

    metodo de mostrar tesst realizados por alumno y test del metodo

--- duocoding/src/main/java/com/codingtrainers/duocoding/repositories/TestExecutionRepository.java
+++ duocoding/src/main/java/com/codingtrainers/duocoding/repositories/TestExecutionRepository.java
@@ -14,4 +14,6 @@ public interface TestExecutionRepository extends JpaRepository<TestExecution, Lo
     List<TestExecution> findByUser(User user);
 
     List<TestExecution> findByTest(Test test);
+
+    List<TestExecution> findByUserId(Long userId);
 }
