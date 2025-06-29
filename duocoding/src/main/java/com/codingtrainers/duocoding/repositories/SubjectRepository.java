fcc5ca0444a2c56715bee39c1e353d7f9539dada
javiBaena <javibaenatro15@gmail.com>
2025-06-29 20:06:18 +0200

    Corrección en SubjectService y SubjectRepository para getAllActive

--- duocoding/src/main/java/com/codingtrainers/duocoding/repositories/SubjectRepository.java
+++ duocoding/src/main/java/com/codingtrainers/duocoding/repositories/SubjectRepository.java
@@ -2,13 +2,13 @@ package com.codingtrainers.duocoding.repositories;
 import com.codingtrainers.duocoding.entities.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
 
 Optional<Subject> findByIdAndActiveTrue(Long id);
-
+List<Subject> findAllByActiveTrue();
 
 }
